f71b4b8a0b3baa57afa43b65967f76662fdae771
thaisc98 <thaisc98@hotmail.es>
2025-02-24 12:53:29 -0400

    [new] first search box componenent

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,4 +1,14 @@
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+import { ChangeEventHandler } from "react";
+
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
+
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
   <div>
     <input
       className={`search-box mt-6 px-4 py-2 leading-7 border-2 rounded-xl border-green-500/75 bg-zinc-900  text-white ${className}`}
